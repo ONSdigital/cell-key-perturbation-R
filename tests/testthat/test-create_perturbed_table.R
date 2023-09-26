d6207a55f98d8c3d1f7a6e2eebca0a57246f3fce
Elinor Everitt <elinor.everitt@ons.gov.uk>
2023-09-26 09:36:46 +0100

    Added unit testing

---
+++ tests/testthat/test-create_perturbed_table.R
@@ -0,0 +1,137 @@
+# =============================================================================
+# -------------------------- TESTING TEMPLATE ---------------------------------
+# =============================================================================
+# Test
+# - 1. type validation on input data & ptable
+# - 2. geog, tab_vars & record_key_arg specified are contained within data
+# - 3. ptable has correct format
+# - 4. perturbed table content matches expected output
+#
+# TEST DATA:
+# perturbed_table_var1_var5_var8.rds - expected output for perturbation on
+#                                      micro data using ptable_10_5,
+#                                      geog=var1 & tab_vars=var5,var8
+# =============================================================================
+
+# -----------------------------------------------------------------------------
+# TESTS: 1. Check type validation on input data & ptable
+# -----------------------------------------------------------------------------
+
+test_that("error raised if input data NOT a data.table", {
+  expect_error(create_perturbed_table(data = "not_a_data_table",
+                                      geog = c("var1"),
+                                      tab_vars = c("var5","var8"),
+                                      record_key_arg = "record_key",
+                                      ptable = ptable_10_5),
+        "Specified value for data must be a data.table", fixed = TRUE)
+})
+
+test_that("error raised if ptable supplied NOT a data.table", {
+  expect_error(create_perturbed_table(data = micro,
+                                      geog = c("var1"),
+                                      tab_vars = c("var5","var8"),
+                                      record_key_arg = "record_key",
+                                      ptable = "not_a_data_table"),
+        "Specified value for ptable must be a data.table", fixed = TRUE)
+})
+
+# -----------------------------------------------------------------------------
+# TESTS: 2. Check errors raised if geog, tab_vars & record_key_arg not within data
+# -----------------------------------------------------------------------------
+
+test_that("error raised if geog not a column within data", {
+  expect_error(create_perturbed_table(data = micro,
+                                      geog = c("not_col_in_data"),
+                                      tab_vars = c("var5","var8"),
+                                      record_key_arg = "record_key",
+                                      ptable = ptable_10_5),
+        "Specified value for geog must be a column name in data.",
+        fixed = TRUE)
+})
+
+test_that("error raised if tab_vars not columns within data", {
+  expect_error(create_perturbed_table(data = micro,
+                                      geog = c("var1"),
+                                      tab_vars = c("not_col_in_data","var8"),
+                                      record_key_arg = "record_key",
+                                      ptable = ptable_10_5),
+        "Specified values for tab_vars must be column names in data.",
+        fixed = TRUE)
+})
+
+test_that("error raised if record_key_arg not a column within data", {
+  expect_error(create_perturbed_table(data = micro,
+                                      geog = c("var1"),
+                                      tab_vars = c("var5","var8"),
+                                      record_key_arg = "not_record_key",
+                                      ptable = ptable_10_5),
+          "Specified value for record_key_arg must be a column name in data.",
+          fixed = TRUE)
+})
+
+# -----------------------------------------------------------------------------
+# TESTS: 3. Check errors raised if ptable has incorrect format
+# -----------------------------------------------------------------------------
+
+dodgy_ptable = data.table(
+  pcv = 1:5,
+  ckey = 6:10,
+  zvalue = 11:15
+)
+
+test_that("error raised if ptable does not contain required column names", {
+  expect_error(create_perturbed_table(data = micro,
+                                      geog = c("var1"),
+                                      tab_vars = c("var5","var8"),
+                                      record_key_arg = "record_key",
+                                      ptable = dodgy_ptable),
+          "Supplied ptable must contain columns called 'pcv','ckey' and 'pvalue'",
+          fixed = TRUE)
+})
+
+# -----------------------------------------------------------------------------
+# TESTS: 4. Check perturbed table content matches expected output
+# -----------------------------------------------------------------------------
+
+# Create perturbed table for micro data using geog=var1 & tab_vars=var5,var8
+# which includes zero count combinations but should return a table with all
+# combinations i.e. 200 rows
+test_that("Perturbed table includes zero count combinations", {
+
+  result <- create_perturbed_table(data = micro,
+                                   geog = c("var1"),
+                                   tab_vars = c("var5","var8"),
+                                   record_key_arg = "record_key",
+                                   ptable = ptable_10_5)
+
+  expect_equal(nrow(result),200)
+})
+
+
+# Create perturbed table for micro data with geog=var1 & tab_vars=var5,var8
+# and check columns, including counts, match those from python
+# implementation of ckp
+test_that("Perturbed table has expected counts", {
+
+  result <- create_perturbed_table(data = micro,
+                                   geog = c("var1"),
+                                   tab_vars = c("var5","var8"),
+                                   record_key_arg = "record_key",
+                                   ptable = ptable_10_5)
+
+
+  # Load expected_result from rda file, and sort data for comparison
+  load(file = "perturbed_table_var1_var5_var8.rda")
+  grouping_cols <- c("var1","var5","var8")
+  expected_result[,(grouping_cols):=lapply(.SD,as.character),.SDcols=grouping_cols]
+  setorderv(expected_result,grouping_cols)
+  setorderv(result,grouping_cols)
+
+  # Compare calculated columns, including counts
+  expect_identical(result$ckey,expected_result$ckey)
+  expect_identical(result$pcv,expected_result$pcv)
+  expect_identical(result$pre_sdc_count,expected_result$pre_sdc_count)
+  expect_identical(result$pvalue,expected_result$pvalue)
+  expect_identical(result$count,expected_result$count)
+
+})
